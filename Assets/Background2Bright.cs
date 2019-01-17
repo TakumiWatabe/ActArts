990bf6584f7f6227e5ed704182c4776a75d36c8a
Iriyama Shou <bash.vanish78@gmail.com>
2019-01-17 16:30:58 +0900

    ビルの明かり、勝利ポーズ、倒れるモーションを今あるキャラ(Aoi,Hikari,Xion)のanimatorに実装

---
+++ Assets/Background2Bright.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background2Bright : MonoBehaviour {
+
+    Material materials;
+    Color col;
+    float alpha = 0;
+    [SerializeField]
+    float alphaPlus = 0.01f;
+
+    // Use this for initialization
+    void Start () {
+        materials = this.GetComponent<MeshRenderer>().materials[0];
+        col = new Color(1, 1, 1, 1);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        col = new Color(1, 1, 1, Mathf.Cos(alpha) / 2 + 0.5f);
+        //col.a = Mathf.Cos(alpha) / 2 + 0.5f;
+        alpha += alphaPlus;
+        materials.color = col;
+    }
+}
