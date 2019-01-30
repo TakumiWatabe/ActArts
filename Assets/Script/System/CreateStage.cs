0ca215f9b02758b8178901fa62a3b3fd16b0c3fe
watabe <takumi.watabe.program@gmail.com>
2019-01-30 12:20:53 +0900

    ステージのランダム生成、エフェクトの修正

---
+++ Assets/Script/System/CreateStage.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateStage : MonoBehaviour {
+    [SerializeField]
+    GameObject stage1;
+    [SerializeField]
+    GameObject stage2;
+    [SerializeField]
+    GameObject stage3;
+
+    int rnd;
+    GameObject mainStage;
+	// Use this for initialization
+	void Start () {
+        rnd = Random.Range(1, 3);
+        if (GameObject.FindGameObjectWithTag("stage") == null)
+        {
+            ChoiceStage(rnd);
+            Instantiate(mainStage);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+    public void ChoiceStage(int num)
+    {
+        switch(num)
+        {
+            case 1:
+                mainStage = stage1;
+                break;
+            case 2:
+                mainStage = stage2;
+                break;
+            case 3:
+                mainStage = stage3;
+                break;
+        }
+    }
+}
